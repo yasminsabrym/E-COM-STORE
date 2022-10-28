417ad83b851c6ab3c314eb9862786b6e9a67169d
Yasmin <yazzmin0011@gmail.com>
2022-10-29 00:56:13 +0200

    Homapage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import { Routes, Route } from "react-router-dom";
+import Products from "../src/Components/Products";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+
+        {/* <Home /> */}
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Products from "./Products";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white border-0 ">
+        <img
+          src="../../Assets/bg.jpg"
+          className="card-img"
+          alt="..."
+          height="600vh"
+          //   width="100px"
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              NEW SEASON ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE NEW TRENDS</p>
+          </div>
+        </div>
+      </div>
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
